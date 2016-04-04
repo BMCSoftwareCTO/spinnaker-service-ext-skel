cf8b2fa746e0872588c7b2a5f00a855467873408
cholin-bmc <Chowie_Lin@bmc.com>
2016-04-05 00:07:21 +0100

    Restart service if it was running

--- spinnakersvc-web/pkg_scripts/postUninstall.sh
+++ spinnakersvc-web/pkg_scripts/postUninstall.sh
@@ -1,6 +1,21 @@
-#!/bin/sh
+#!/bin/bash
+service=#APPNAME#
+isRunning=0
 
-echo updating #APPNAME# shell script CLASSPATH
-sed -i.bak -e '/CLASSPATH=\$CLASSPATH:#INSTALLPATH_ESCAPED#\/lib\/\*/d' -e s/"\(DEFAULT_JVM_OPTS=\)'\(.*\)\"\(\-Dspring\.config\.location=\)\(.*\)\(,#INSTALLPATH_ESCAPED#\\/config\\/\)\"'"/\\1\'\\2\"\\3\\4\"\'/ #SERVICE_INSTALLPATH#/bin/#APPNAME#
-echo start #APPNAME# service
-service #APPNAME# start
+if (( $(ps -ef | grep -v grep | grep $service | wc -l) > 0 ))
+then
+  echo "$service is running"
+  isRunning=1
+else
+  echo "$service is NOT running"
+  isRunning=0
+fi
+
+
+echo Updating $service shell script
+sed -i.bak -e '/CLASSPATH=\$CLASSPATH:#INSTALLPATH_ESCAPED#\/lib\/\*/d' -e s/"\(DEFAULT_JVM_OPTS=\)'\(.*\)\"\(\-Dspring\.config\.location=\)\(.*\)\(,#INSTALLPATH_ESCAPED#\\/config\\/\)\"'"/\\1\'\\2\"\\3\\4\"\'/ #SERVICE_INSTALLPATH#/bin/$service
+
+if [ "$isRunning" = "1" ]; then
+  echo Restarting $service service
+  service $service restart
+fi
