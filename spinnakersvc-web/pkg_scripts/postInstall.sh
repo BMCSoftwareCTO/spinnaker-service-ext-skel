9ef21a6b1a435108d50c4bd03381a6d9a06b77a2
cholin-bmc <Chowie_Lin@bmc.com>
2016-04-04 22:21:13 +0100

    Add config path to spring.config.location JVM property

--- spinnakersvc-web/pkg_scripts/postInstall.sh
+++ spinnakersvc-web/pkg_scripts/postInstall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 echo updating #APPNAME# shell script CLASSPATH
-sed -i.bak s/"\(CLASSPATH=\\\$APP_HOME.*\)"/\\1\\nCLASSPATH=\$CLASSPATH:#INSTALLPATH_ESCAPED#\\/config:#INSTALLPATH_ESCAPED#\\/lib\\/\\*/ #SERVICE_INSTALLPATH#/bin/#APPNAME#
+sed -i.bak -e s/"\(CLASSPATH=\\\$APP_HOME.*\)"/\\1\\nCLASSPATH=\$CLASSPATH:#INSTALLPATH_ESCAPED#\\/lib\\/\\*/ -e s/"\(DEFAULT_JVM_OPTS=\)'\(.*\)\"\(\-Dspring\.config\.location=\)\(.*\)\"'"/\\1\'\\2\"\\3\\4,#INSTALLPATH_ESCAPED#\\\/config\\/\"\'/ #SERVICE_INSTALLPATH#/bin/#APPNAME#
 echo start #APPNAME# service
 service #APPNAME# start
--- spinnakersvc-web/pkg_scripts/postUninstall.sh
+++ spinnakersvc-web/pkg_scripts/postUninstall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 echo updating #APPNAME# shell script CLASSPATH
-sed -i.bak '/CLASSPATH=\$CLASSPATH:#INSTALLPATH_ESCAPED#\/config:#INSTALLPATH_ESCAPED#\/lib\/\*/d' #SERVICE_INSTALLPATH#/bin/#APPNAME#
+sed -i.bak -e '/CLASSPATH=\$CLASSPATH:#INSTALLPATH_ESCAPED#\/lib\/\*/d' -e s/"\(DEFAULT_JVM_OPTS=\)'\(.*\)\"\(\-Dspring\.config\.location=\)\(.*\)\(,#INSTALLPATH_ESCAPED#\\/config\\/\)\"'"/\\1\'\\2\"\\3\\4\"\'/ #SERVICE_INSTALLPATH#/bin/#APPNAME#
 echo start #APPNAME# service
 service #APPNAME# start
